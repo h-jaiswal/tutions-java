2b9927c2da6abb672b5bc10286a8777e97919087
Gunjan Pande <88021616+GunjanPande@users.noreply.github.com>
2021-08-02 18:43:55 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int a,b;
+	    a=24;
+	    b=12;
+	    if(a==b)
+		    system.out.println// write your code here
     }
 }
