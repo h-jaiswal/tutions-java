dbdd29bf5fc4b45836b9089cc3f0d224932824f6
h-jaiswal <54812359+h-jaiswal@users.noreply.github.com>
2021-08-02 18:46:03 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int a,b;
-	    a=24;
-	    b=12;
-	    if(a==b)
-		    system.out.println// write your code here
+	int a = 10;
+	int b = 10;
+	if (a == b) {
+		System.out.println("Equal");
+	}
     }
 }
