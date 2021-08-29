625ac1274a58abf5b5ea64c65efae6901ee5deb4
h-jaiswal <mr.harshit.jaiswal@gmail.com>
2021-08-29 12:27:40 +0530

    Aug 29 - if, Check Prime number using 5 techniques

--- src/com/company/Lecture4/UnderstandingLoops.java
+++ src/com/company/Lecture4/UnderstandingLoops.java
@@ -1,4 +1,38 @@
 package com.company.Lecture4;
 
 public class UnderstandingLoops {
+    public static void main(String[] args) {
+
+        int k = 1;
+        while (k <= 10) {
+            System.out.println(k);
+
+            k++;
+        }
+
+
+        System.out.println();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+
+        System.out.println();
+        int i = 1;
+        do {
+            System.out.println(i);
+
+            i++;
+        } while (i <= 10);
+
+
+        System.out.println();
+        int j = 11;
+        do {
+            System.out.println(j);
+
+            j++;
+        } while (j <= 10);
+
+    }
 }
--- src/com/company/Lecture5/Prime.java
+++ src/com/company/Lecture5/Prime.java
@@ -1,4 +1,162 @@
 package com.company.Lecture5;
 
+import java.util.Scanner;
+
 public class Prime {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void solve1() {
+        int num = scanner.nextInt();
+
+        int numFactors = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                numFactors++;
+            }
+        }
+
+        if (numFactors == 2) {
+            System.out.printf("%d is a prime number", num);
+        } else {
+            System.out.printf("%d is NOT a prime number", num);
+        }
+    }
+
+    public static void solve2() {
+//        better than METHOD 1
+        int num = scanner.nextInt();
+
+        if (num <= 1) {   // Edge Case
+            System.out.printf("%d is NOT a prime number", num);
+            return;    // terminates the method
+        }
+
+        boolean isPrime = true;  // assume num is prime
+
+        for (int i = 2; i <= num-1; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;   // num is NOT prime
+            }
+        }
+
+        if (isPrime) {
+            System.out.printf("%d is a prime number", num);
+        } else {
+            System.out.printf("%d is NOT a prime number", num);
+        }
+    }
+
+    public static void solve4() {
+//        better than METHOD 2
+        int num = scanner.nextInt();
+
+        if (num <= 1) {   // Edge Case
+            System.out.printf("%d is NOT a prime number", num);
+            return;    // terminates the method
+        }
+
+        boolean isPrime = true;  // assume num is prime
+
+        for (int i = 2; i <= num/2; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;   // num is NOT prime
+            }
+        }
+
+        if (isPrime) {
+            System.out.printf("%d is a prime number", num);
+        } else {
+            System.out.printf("%d is NOT a prime number", num);
+        }
+    }
+
+
+    public static void solve5() {
+//        better than METHOD 4
+        int num = scanner.nextInt();
+
+        if (num <= 1) {   // Edge Case
+            System.out.printf("%d is NOT a prime number", num);
+            return;    // terminates the method
+        }
+
+        boolean isPrime = true;  // assume num is prime
+
+//        for (int i = 2; i <= (int)(Math.sqrt(num)); i++) {
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;   // num is NOT prime
+            }
+        }
+
+        if (isPrime) {
+            System.out.printf("%d is a prime number", num);
+        } else {
+            System.out.printf("%d is NOT a prime number", num);
+        }
+    }
+
+
+    public static boolean ContainsNoDecimalPart(double n) {
+        return ( n == (int)(n) );
+//
+//        if ( n == (int)(n) ) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+
+//        if ( n == (int)(n) ) {
+//            return true;
+//        }
+//        return false;
+    }
+    public static void solve3() {
+        int num = scanner.nextInt();
+
+        if ( num <= 1) {  // Edge Case 1
+            System.out.printf("%d is NOT a prime number", num);
+            return;
+        }
+
+        if (num == 2 || num == 3) {   // Edge Case 2
+            System.out.printf("%d is a prime number", num);
+            return;
+        }
+
+        double n1 = (num-1)/6d;
+        double n2 = (num+1)/6d;
+
+        if ( ContainsNoDecimalPart(n1) || ContainsNoDecimalPart(n2) ) {
+            System.out.printf("%d is a prime number", num);
+        } else {
+            System.out.printf("%d is NOT a prime number", num);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        solve1();
+
+        System.out.println();
+
+        solve2();
+
+        System.out.println();
+
+        solve4();
+
+        System.out.println();
+
+        solve5();
+
+        System.out.println();
+
+        solve3();
+
+        scanner.close();
+    }
 }
